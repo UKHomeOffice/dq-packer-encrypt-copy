719ec248018e3ab9bc29ef0f8f25e120d4adc7bb
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-01-05 14:14:12 +0000

    fix sort and take last in list not first

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-$(aws ec2 describe-images --region ${region} ${filters} --query 'Images[*].[CreationDate, ImageId, Name]' --output text | sort -k2 | head -n1 >/tmp/build)
+$(aws ec2 describe-images --region ${region} ${filters} --query 'Images[*].[CreationDate, ImageId, Name]' --output text | sort -k1 | tail -n1 >/tmp/build)
 
 export created=$(cat /tmp/build | awk  -F "\t" '{{print $1}}')
 export ami_id=$(cat /tmp/build | awk  -F "\t" '{{print $2}}')
