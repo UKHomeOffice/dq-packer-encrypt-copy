1fafa2e1c46ba3b6df081d33f5b590f8760af398
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-01-05 15:09:46 +0000

    force the env vars right so awscli can find the image

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export AWS_ACCESS_KEY_ID=${aws_id}
+export AWS_SECRET_ACCESS_KEY=${aws_key}
+
 $(aws ec2 describe-images --region ${region} ${filters} --query 'Images[*].[CreationDate, ImageId, Name]' --output text | sort -k1 | tail -n1 >/tmp/build)
 
 export created=$(cat /tmp/build | awk  -F "\t" '{{print $1}}')
