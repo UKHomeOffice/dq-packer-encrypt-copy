e5c337a93ae2a93983ab5dbc0f57d7e88a28ac2a
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2023-03-20 16:48:14 +0000

    Display account id - not region (which does not really change). Hide sensitive variables.

--- build.sh
+++ build.sh
@@ -5,10 +5,13 @@ export AWS_SECRET_ACCESS_KEY=${aws_key}
 
 $(aws ec2 describe-images --region ${region} ${filters} --query 'Images[*].[CreationDate, ImageId, Name]' --output text | sort -k1 | tail -n1 >/tmp/build)
 
+$(aws sts get-caller-identity --output text > /tmp/build-account)
+
 export created=$(cat /tmp/build | awk  -F "\t" '{{print $1}}')
 export ami_id=$(cat /tmp/build | awk  -F "\t" '{{print $2}}')
 export ami_name=$(cat /tmp/build | awk  -F "\t" '{{print $3}}')
+export account=$(cat /tmp/build-account | awk -F "\t" '{{print $1}}')
 
-echo Found: ${ami_id} -  \<${ami_name}\> created ${created} will now make an encrypted copy of this into: ${region}
+echo Found: ${ami_id} - \<${ami_name}\> created ${created} will now make an encrypted copy of this account: ${account}
 
 packer build packer.json
