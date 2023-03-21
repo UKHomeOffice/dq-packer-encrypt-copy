91830afcd2e9b21117c8712c7befb019c5d9db42
Ian Relf <36162543+ianrelf@users.noreply.github.com>
2023-03-21 13:55:06 +0000

    Feature/yel 6839 update versions (#7)
    * Re-add aws-cli tools, accidentally removed earlier.
    
    * Display account id - not region (which does not really change).
    Hide sensitive variables.
    
    * Add region arg.
    
    * Added diags.
    
    * Fix typo.
    
    * Region should not be required for get-caller-identity.
    
    * Improve formatting.

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
+echo "region = ${region}"
+echo "filters = ${filters}"
+
 export AWS_ACCESS_KEY_ID=${aws_id}
 export AWS_SECRET_ACCESS_KEY=${aws_key}
 
-$(aws ec2 describe-images --region ${region} ${filters} --query 'Images[*].[CreationDate, ImageId, Name]' --output text | sort -k1 | tail -n1 >/tmp/build)
+$(aws ec2 describe-images --region ${region} ${filters} --query 'Images[*].[CreationDate, ImageId, Name]' --output text | sort -k1 | tail -n1 > /tmp/build)
+
+$(aws sts get-caller-identity --output text > /tmp/build-account)
+
 
 export created=$(cat /tmp/build | awk  -F "\t" '{{print $1}}')
 export ami_id=$(cat /tmp/build | awk  -F "\t" '{{print $2}}')
 export ami_name=$(cat /tmp/build | awk  -F "\t" '{{print $3}}')
+export account=$(cat /tmp/build-account | awk -F "\t" '{{print $1}}')
 
-echo Found: ${ami_id} -  \<${ami_name}\> created ${created} will now make an encrypted copy of this into: ${region}
+echo "Found AMI: (ID: <${ami_id}> Name: <${ami_name}> Created: <${created}>) will now make an encrypted copy to account: <${account}>"
 
 packer build packer.json
