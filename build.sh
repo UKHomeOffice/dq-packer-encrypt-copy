2aec5c7dfd400975f4bf60a04942841b51f7c4de
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-08-31 17:18:48 +0100

    feat: yel-8016, Reverting back recent change

--- build.sh
+++ build.sh
@@ -17,8 +17,6 @@ export ami_id=$(cat /tmp/build | awk  -F "\t" '{{print $2}}')
 export ami_name=$(cat /tmp/build | awk  -F "\t" '{{print $3}}')
 export account=$(cat /tmp/build-account | awk -F "\t" '{{print $1}}')
 
-$(aws ec2 modify-image-attribute --image-id ${ami_id} --imds-support v2.0)
-
 echo "Found AMI: (ID: <${ami_id}> Name: <${ami_name}> Created: <${created}>)."
 echo "Will now make an encrypted copy to account: <${account}>."
 
